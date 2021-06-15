1b9dccbe4bcc62c861db3edf68857567a0098d16
Pallavi Das <pallavids92@gmail.com>
2021-06-15 22:35:52 +0530

    Create longestSubstring.js

---
+++ longestSubstring.js
@@ -0,0 +1,24 @@
+console.log('Longest substring length');
+
+var lengthOfLongestSubstring = function (s) {
+    var letters = s.split("");
+    var max = 0;
+    var result = new Map();
+    var start = 0;
+
+    for (var i = 0; i < letters.length; i++) {
+        if (!result.has(letters[i])) {
+            result.set(letters[i], i);
+        } else {
+            i = result.get(letters[i]);
+            result.clear();
+        }
+
+        if (max < result.size) {
+            max = result.size;
+        }
+    }
+    return max;
+};
+
+console.log(lengthOfLongestSubstring('abcbb'));
