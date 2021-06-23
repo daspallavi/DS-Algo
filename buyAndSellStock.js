42d793bd998f67aed87ed095dedc0b90998da380
Pallavi Das <pallavids92@gmail.com>
2021-06-23 11:40:59 +0530

    buy and sell

--- buyAndSellStock.js
+++ buyAndSellStock.js
@@ -1,18 +1,35 @@
+// const getMaxProfit = (prices) => {
+//     let maxProfit = 0;
+
+//     for (let buy = 0; buy < prices.length; buy++) {
+//         const buyPrice = prices[buy];
+
+//         for (let sell = buy + 1; sell < prices.length; sell++) {
+//             const sellPrice = prices[sell];
+//             const currentProfit = sellPrice - buyPrice;
+
+//             maxProfit = Math.max(maxProfit, currentProfit);
+//         }
+//     }
+
+//     return maxProfit;
+// };
+
+
+/**approach -2 */
 const getMaxProfit = (prices) => {
-    let maxProfit = 0;
+    let minPrice = prices[0];
+    let maxProft = 0;
 
-    for (let buy = 0; buy < prices.length; buy++) {
-        const buyPrice = prices[buy];
+    for (const price of prices) {
+        const currentProfit = price - minPrice;
+        minPrice = Math.min(minPrice, price);  // buying price
 
-        for (let sell = buy + 1; sell < prices.length; sell++) {
-            const sellPrice = prices[sell];
-            const currentProfit = sellPrice - buyPrice;
+        maxProft = Math.max(maxProft, currentProfit);
 
-            maxProfit = Math.max(maxProfit, currentProfit);
-        }
     }
 
-    return maxProfit;
+    return maxProft;
 };
 
 
